af773829469708fc1ced097328226cd36741aeb4
Serhzik <Serhziken@gmail.com>
2016-11-17 00:51:35 +0700

    Написал первую версию меню.

---
+++ Assets/Scripts/Interface.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+public class Interface : MonoBehaviour {
+
+    public GameObject[] Opened;
+    public GameObject[] Stars1, Stars2, Stars3;
+
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < MenuManager.Instance.LvlOpened; i++)
+        {
+            Opened[i].SetActive(true);
+            if (MenuManager.Instance.Stars[i] == 1)
+                Stars1[i].SetActive(true);
+            else if (MenuManager.Instance.Stars[i] == 2)
+                Stars2[i].SetActive(true);
+            else if (MenuManager.Instance.Stars[i] == 3)
+                Stars3[i].SetActive(true);
+        }
+	}
+    public void OpenRandomStar()
+    {
+        MenuManager.Instance.OpenLvl(Random.Range(1, 3));
+        Debug.Log("OpenRandom");
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+    public static MenuManager Instance;
+
+    public int LvlOpened, MaxLevels;
+    public bool Completed;
+    public int[] Stars;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+    }
+
+	void Start () {
+        LvlOpened = PlayerPrefs.GetInt("Opened", 1);
+        for (int i = 0; i < 10; i++)
+            Stars[i] = PlayerPrefs.GetInt("Stars" + i, 0);
+        if (Stars[9] != 0)
+            Completed = true;
+	}
+	
+	public void OpenLvl(int stars)
+    {
+        Stars[LvlOpened] = stars;
+        PlayerPrefs.SetInt("Stars" + LvlOpened, stars);
+
+        if (LvlOpened <= MaxLevels)
+        {
+            LvlOpened++;
+            PlayerPrefs.SetInt("Opened", LvlOpened);
+        }
+        else
+        {
+            Completed = true;
+        }
+        Debug.Log("Open lvl = " + LvlOpened + "Stars = " + stars);
+    }
+}
