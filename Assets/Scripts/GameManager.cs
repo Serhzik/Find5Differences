6b9645f09499440549a0493c9b33765eab691f55
Serhzik <Serhziken@gmail.com>
2016-11-17 22:51:53 +0700

    Начало разработки меню

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public int FindedDifs;
+    public float time;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+
+	}
+    public void Find()
+    {
+        FindedDifs++;
+        if (FindedDifs == 5) // Win
+        {
+            int stars = 1;
+            if (time < 180)
+                stars++;
+            if (time < 60)
+                stars++;
+            MenuManager.Instance.OpenLvl(stars);
+        }
+    }
+}
